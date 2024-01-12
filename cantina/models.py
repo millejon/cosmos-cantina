e876fb4e7a9747d067a33f3d3283cfa55927c758
Jonathan Miller <millejon@oregonstate.edu>
2024-01-11 18:09:48 -0800

    Add Drink model

--- cantina/models.py
+++ cantina/models.py
@@ -5,7 +5,7 @@ class Customer(models.Model):
     last_name = models.CharField(max_length=100)
     first_name = models.CharField(max_length=100, blank=True)
     planet = models.CharField(max_length=100)
-    uba = models.CharField(max_length=24, blank=True, verbose_name="UBA Number")
+    uba = models.CharField("UBA Number", max_length=24, blank=True)
 
     class Meta:
         unique_together = ["last_name", "first_name"]
@@ -15,3 +15,11 @@ class Customer(models.Model):
             return f"{self.first_name} {self.last_name}"
         else:
             return self.last_name
+
+
+class Drinks(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return self.name
