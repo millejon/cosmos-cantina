5808dbc2494a17da1d32da25e754b5c9982a9766
Jonathan Miller <millejon@oregonstate.edu>
2024-01-11 17:30:44 -0800

    Add Customer model

--- cantina/models.py
+++ cantina/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100, blank=True)
+    planet = models.CharField(max_length=100)
+    uba = models.CharField(max_length=24, blank=True, verbose_name="UBA Number")
+
+    class Meta:
+        unique_together = ["last_name", "first_name"]
+
+    def __str__(self):
+        if self.first_name:
+            return f"{self.first_name} {self.last_name}"
+        else:
+            return self.last_name
