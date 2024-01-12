6f37e117d8e172e8406929b3c634883be8335c59
Jonathan Miller <millejon@oregonstate.edu>
2024-01-11 18:20:45 -0800

    Add Ingredient model

--- cantina/admin.py
+++ cantina/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Customer, Drink
+from . import models
 
-admin.site.register(Customer)
-admin.site.register(Drink)
+admin.site.register(models.Customer)
+admin.site.register(models.Drink)
+admin.site.register(models.Ingredient)
--- cantina/models.py
+++ cantina/models.py
@@ -27,3 +27,14 @@ class Drink(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    stock = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
