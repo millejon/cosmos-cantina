fe04dad5d6bce5e5cbfc12de85a0b160cbe425b2
Jonathan Miller <millejon@oregonstate.edu>
2024-01-11 18:41:36 -0800

    Add Tab model

--- cantina/models.py
+++ cantina/models.py
@@ -51,3 +51,17 @@ class Recipe(models.Model):
 
     def __str__(self):
         return f"{self.drink.name} - {self.ingredient.name}: {self.amount}"
+
+
+class Tab(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    opened = models.DateTimeField()
+    due = models.DateTimeField()
+    closed = models.DateTimeField(null=True)
+
+    class Meta:
+        unique_together = ["customer", "opened"]
+        ordering = ["customer__last_name", "-opened"]
+
+    def __str__(self):
+        return f"{self.customer.last_name}: {self.opened}"
