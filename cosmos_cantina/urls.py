d8e9fc6564d37ead35a77ac719cb4bd6773e84b5
Jonathan Miller <millejon@oregonstate.edu>
2024-01-13 16:17:27 -0800

    Update URL patterns

--- cosmos_cantina/urls.py
+++ cosmos_cantina/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("cantina/", include("cantina.urls")),
+    path("", include("cantina.urls")),
     path("admin/", admin.site.urls),
 ]
