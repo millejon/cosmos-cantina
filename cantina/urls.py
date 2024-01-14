6f3db558aa9fe37e30810d35c59f12f18653b457
Jonathan Miller <millejon@oregonstate.edu>
2024-01-13 17:54:43 -0800

    Add customer detail view

--- cantina/urls.py
+++ cantina/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = "cantina"
 urlpatterns = [
     path("customers/", views.all_customers, name="all_customers"),
+    path("customers/<int:customer_id>/", views.customer, name="customer"),
 ]
--- cantina/views.py
+++ cantina/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from . import models
 
 
-def view_all_customers(request):
+def all_customers(request):
     customers = models.Customer.objects.all()
     context = {"customers": customers}
     return render(request, "cantina/all_customers.html", context)
+
+
+def customer(request, customer_id):
+    customer = get_object_or_404(models.Customer, pk=customer_id)
+    tabs = customer.tab_set.all()
+    context = {"customer": customer, "tabs": tabs}
+    return render(request, "cantina/customer.html", context)
