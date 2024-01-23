c1f6d54432a6682fcf838dfa231af70ff524b0b3
Jonathan Miller <millejon@oregonstate.edu>
2024-01-23 14:21:18 -0800

    Add test for Customer model method

--- cantina/tests.py
+++ cantina/tests.py
@@ -1,1 +1,20 @@
-# from django.test import TestCase
+from django.test import TestCase
+
+from . import models
+
+
+class CustomerTestCase(TestCase):
+    def setUp(self):
+        models.Customer.objects.create(
+            last_name="Radd",
+            first_name="Norrin",
+            planet="Zenn-La",
+            uba="OHCMSZ9QF928JZNS4H20IKXP",
+        )
+        models.Customer.objects.create(last_name="Galactus", planet="Galan")
+
+    def test_customer_name(self):
+        silver_surfer = models.Customer.objects.get(last_name="Radd")
+        galactus = models.Customer.objects.get(last_name="Galactus")
+        self.assertEqual(silver_surfer.name, "Norrin Radd")
+        self.assertEqual(galactus.name, "Galactus")
