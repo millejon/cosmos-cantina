467e026e5c847e108c8e73efa6f066fb2922d042
Jonathan Miller <millejon@oregonstate.edu>
2024-01-23 15:35:05 -0800

    Update tests for Tab model methods

--- cantina/tests.py
+++ cantina/tests.py
@@ -28,16 +28,20 @@ class CustomerTestCase(TestCase):
 
 class TabTestCase(TestCase):
     def setUp(self):
-        customer = models.Customer.objects.create(last_name="Thanos", planet="Titan")
-        models.Tab.objects.create(customer=customer)
+        self.customer = models.Customer.objects.create(
+            last_name="Thanos", planet="Titan"
+        )
+        self.tab = models.Tab.objects.create(customer=self.customer)
         category = models.MenuItemCategory.objects.create(name="Beer")
-        models.MenuItem.objects.create(name="Duff Beer", category=category, price=5)
+        self.beer = models.MenuItem.objects.create(
+            name="Duff Beer", category=category, price=5
+        )
 
-    def make_purchase(self, tab: models.Tab) -> None:
-        """Create a Purchase object assigned to the tab passed."""
+    def make_purchase(self) -> None:
+        """Create a Purchase object."""
         models.Purchase.objects.create(
-            tab=tab,
-            item=models.MenuItem.objects.get(name="Duff Beer"),
+            tab=self.tab,
+            item=self.beer,
             quantity=2,
             amount=10,
         )
@@ -48,12 +52,9 @@ class TabTestCase(TestCase):
         number of purchases assigned to the tab. A tab with a single
         purchase should return 1.
         """
-        tab = models.Tab.objects.get(
-            customer=models.Customer.objects.get(last_name="Thanos")
-        )
-        self.make_purchase(tab)
+        self.make_purchase()
 
-        self.assertEqual(len(tab.get_purchases()), 1)
+        self.assertEqual(len(self.tab.get_purchases()), 1)
 
     def test_tab_get_purchases_with_multiple_purchases(self):
         """
@@ -61,13 +62,10 @@ class TabTestCase(TestCase):
         number of purchases assigned to the tab. A tab with five
         purchases should return 5.
         """
-        tab = models.Tab.objects.get(
-            customer=models.Customer.objects.get(last_name="Thanos")
-        )
         for _ in range(5):
-            self.make_purchase(tab)
+            self.make_purchase()
 
-        self.assertEqual(len(tab.get_purchases()), 5)
+        self.assertEqual(len(self.tab.get_purchases()), 5)
 
     def test_tab_get_purchases_with_no_purchases(self):
         """
@@ -75,36 +73,26 @@ class TabTestCase(TestCase):
         number of purchases assigned to the tab. A tab with no purchases
         should return 0.
         """
-        tab = models.Tab.objects.get(
-            customer=models.Customer.objects.get(last_name="Thanos")
-        )
-
-        self.assertEqual(len(tab.get_purchases()), 0)
+        self.assertEqual(len(self.tab.get_purchases()), 0)
 
     def test_tab_get_amount_with_single_purchase(self):
         """
         The get_amount methods of the Tab model should return the total
         price of all purchases assigned to the tab.
         """
-        tab = models.Tab.objects.get(
-            customer=models.Customer.objects.get(last_name="Thanos")
-        )
-        self.make_purchase(tab)
+        self.make_purchase()
 
-        self.assertEqual(tab.get_amount(), 10)
+        self.assertEqual(self.tab.get_amount(), 10)
 
     def test_tab_get_amount_with_multiple_purchase(self):
         """
         The get_amount methods of the Tab model should return the total
         price of all purchases assigned to the tab.
         """
-        tab = models.Tab.objects.get(
-            customer=models.Customer.objects.get(last_name="Thanos")
-        )
         for _ in range(5):
-            self.make_purchase(tab)
+            self.make_purchase()
 
-        self.assertEqual(tab.get_amount(), 50)
+        self.assertEqual(self.tab.get_amount(), 50)
 
     def test_tab_get_amount_with_no_purchase(self):
         """
@@ -112,8 +100,4 @@ class TabTestCase(TestCase):
         price of all purchases assigned to the tab. A tab with no
         purchases should return 0.
         """
-        tab = models.Tab.objects.get(
-            customer=models.Customer.objects.get(last_name="Thanos")
-        )
-
-        self.assertEqual(tab.get_amount(), 0)
+        self.assertEqual(self.tab.get_amount(), 0)
